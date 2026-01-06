b24ffb20e45252e615000e2e2357553adf214537
Athesh Kumar <atheshpramasivam6725@gmail.com>
2026-01-06 19:14:26 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -8,10 +8,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 from imblearn.over_sampling import SMOTE
 
-
-# -------------------------------
 # Project paths (VERY IMPORTANT)
-# -------------------------------
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = r"C:\Users\Athesh\Downloads\predictive_maintenance\Predictive_maintenance_synthetic.csv"
 MODELS_DIR = os.path.join(BASE_DIR, "models")
@@ -20,25 +18,20 @@ MODELS_DIR = os.path.join(BASE_DIR, "models")
 os.makedirs(MODELS_DIR, exist_ok=True)
 
 
-# -------------------------------
 # 1. Load dataset
-# -------------------------------
+
 df = pd.read_csv(DATA_PATH)
 
-
-# -------------------------------
 # 2. Preprocessing
-# -------------------------------
+
 df = df.drop(columns=["UDI", "Product ID", "Failure Type"])
 df["Type"] = df["Type"].map({"L": 0, "M": 1, "H": 2})
 
 X = df.drop("Target", axis=1)
 y = df["Target"]
 
-
-# -------------------------------
 # 3. Train-test split
-# -------------------------------
+
 X_train, X_test, y_train, y_test = train_test_split(
     X, y,
     test_size=0.2,
@@ -46,27 +39,21 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
-
-# -------------------------------
 # 4. Scaling
-# -------------------------------
+
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-
-# -------------------------------
 # 5. SMOTE
-# -------------------------------
+
 smote = SMOTE(random_state=42)
 X_train_smote, y_train_smote = smote.fit_resample(
     X_train_scaled, y_train
 )
 
-
-# -------------------------------
 # 6. Train model
-# -------------------------------
+
 model = RandomForestClassifier(
     n_estimators=200,
     random_state=42,
@@ -75,10 +62,8 @@ model = RandomForestClassifier(
 
 model.fit(X_train_smote, y_train_smote)
 
-
-# -------------------------------
 # 7. Evaluation
-# -------------------------------
+
 y_pred = model.predict(X_test_scaled)
 
 print("\nClassification Report:\n")
@@ -87,10 +72,8 @@ print(classification_report(y_test, y_pred))
 print("\nConfusion Matrix:\n")
 print(confusion_matrix(y_test, y_pred))
 
-
-# -------------------------------
 # 8. Save artifacts (CRITICAL)
-# -------------------------------
+
 joblib.dump(model, os.path.join(MODELS_DIR, "predictive_maintenance_rf_model.pkl"))
 joblib.dump(scaler, os.path.join(MODELS_DIR, "scaler.pkl"))
 joblib.dump(X.columns.tolist(), os.path.join(MODELS_DIR, "feature_names.pkl"))
